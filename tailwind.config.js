9ff6bdb566c0f3d04ccdf2bcca2c20429eac8e61
Your Name <you@example.com>
2026-02-27 16:48:53 +0800

    Format markdown for Hero Tavern and Quicksand Dock

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
   plugins: [
-    require('@tailwindcss/typography'),
+    require('@tailwindcss/typography'), // 必须有这一行！
   ],
 }
